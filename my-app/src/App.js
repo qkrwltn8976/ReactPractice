063c2161fcc1971525243e54d0b1f33936755c9a
qkrwltn8976 <qkrwltn8976@naver.com>
2020-04-11 16:26:25 +0900

    댓글 컴포넌트 만들기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comment';
 
+const comments = [
+  { name: 'Jisu Park', content: 'My comment!' },
+  { name: '현빈', content: '이게 최선입니까? 확실해요?' },
+  { name: '리정혁', content: '세리동무-' }
+]
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{padding: 16, backgroundColor: '#282c34'}}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +25,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        {comments.map((comment, index) => {
+          return (
+            <Comment
+              name={comment.name}
+              content={comment.content}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,22 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { render } from '@testing-library/react';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// ); 
-// --> JSX로 변환
-// const formatName = function (name) {
-//   return name.firstName + " " + name.lastName;
-// }
-// const name = {
-//   firstName: "JISU",
-//   lastName: "PARK"
-// }
-// const element = <h1>Hello, {formatName(name)}!</h1>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); 
 
+/** JSX 코드 작성해보기
+--> JSX로 변환
+const formatName = function (name) {
+  return name.firstName + " " + name.lastName;
+}
+const name = {
+  firstName: "JISU",
+  lastName: "PARK"
+}
+const element = <h1>Hello, {formatName(name)}!</h1>
+*/
+
+/** 시계 만들기
 function tick () {
   const element = (
     <div>
@@ -35,6 +39,7 @@ function tick () {
 
 setInterval(tick, 1000);
 
+ */
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
