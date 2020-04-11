d623164560eddfc8197614f98e44cec948ae6af3
qkrwltn8976 <qkrwltn8976@naver.com>
2020-04-11 16:41:30 +0900

    state 사용해보기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,39 +3,76 @@ import logo from './logo.svg';
 import './App.css';
 import Comment from './Comment';
 
+/** 댓글 컴포넌트 만들기
 const comments = [
   { name: 'Jisu Park', content: 'My comment!' },
   { name: '현빈', content: '이게 최선입니까? 확실해요?' },
   { name: '리정혁', content: '세리동무-' }
 ]
-function App() {
-  return (
-    <div className="App" style={{padding: 16, backgroundColor: '#282c34'}}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+ */
+const commentsFromServer = [
+  { name: 'Jisu Park', content: 'My comment!' },
+  { name: '현빈', content: '이게 최선입니까? 확실해요?' },
+  { name: '리정혁', content: '세리동무-' }
+];
+
+var timer;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: [],
+    };
+  }
+
+  componentDidMount() {
+    let comments = this.state.comments;
+    timer = setInterval(() => {
+      if (comments.length < commentsFromServer.length) {
+        let index = comments.length;
+        comments.push(commentsFromServer[index]);
+        this.setState({
+          comments: comments
+        })
+      } else if (timer) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+
+  render() {
+    const { comments } = this.state;
+    return (
+      <div className="App" style={{ padding: 16, backgroundColor: '#282c34' }}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-      <div>
-        {comments.map((comment, index) => {
-          return (
-            <Comment
-              name={comment.name}
-              content={comment.content}/>
-          )
-        })}
+        </header>
+        <div>
+          {comments.map((comment, index) => {
+            return (
+              <Comment
+                name={comment.name}
+                content={comment.content} />
+            )
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
