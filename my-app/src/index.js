4e650eac786d35ec8ec1f026db82760b359846a6
qkrwltn8976 <qkrwltn8976@naver.com>
2020-04-11 15:48:04 +0900

    시계 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,18 +12,28 @@ import { render } from '@testing-library/react';
 //   document.getElementById('root')
 // ); 
 // --> JSX로 변환
-const formatName = function (name) {
-  return name.firstName + " " + name.lastName;
-}
-const name = {
-  firstName: "JISU",
-  lastName: "PARK"
-}
-const element = <h1>Hello, {formatName(name)}!</h1>
+// const formatName = function (name) {
+//   return name.firstName + " " + name.lastName;
+// }
+// const name = {
+//   firstName: "JISU",
+//   lastName: "PARK"
+// }
+// const element = <h1>Hello, {formatName(name)}!</h1>
 
-ReactDOM.render(
-  element, 
-  document.getElementById('root'));
+function tick () {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element, 
+    document.getElementById('root'));  
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
