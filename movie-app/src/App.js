5b2a0a08d82010b8deed7679096e6cc55bae74ba
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-05-27 17:04:32 +0900

    2020.05.27
    movie app

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,38 +5,49 @@ import Movie from './Movie';
 
 
 class App extends Component {
-  state = {
-  };
+  state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [{
-            title: "강동원",
-            poster: "https://www.slist.kr/news/photo/201807/40538_104436_3545.jpg"
-          },
-          {
-            title: "현빈",
-            poster: "https://pbs.twimg.com/profile_images/1224596345933066240/px-77DwV_400x400.jpg"
-          }, 
-          {
-            title: "이민기",
-            poster:  "https://lh3.googleusercontent.com/proxy/eMo-D1R-fPi9Z9wQIAh5bRQx56VR5-z7UHXY2jN9NY_t4UaSjtSl52msh3K7VQWiAFJ_dlUP3xn14mVhDpSRQyN6uUGwUhadrBs0dzjfK8qMRQG3RxfjfxIu4ebmI8wZHfmOKjHxRxZJbMKcYA"
-          }
-        ]
-      })
-    }, 3000)
+    this._getMovies();
+    // fetch('https://yts.mx/api/v2/list_movies.json?sort_by=like_count')
+    // .then(response => response.json())
+    // .then(json => {
+    //   this.setState({
+    //     movies: json
+    //   })
+    // })
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
     });
 
     return movies;
 
     // 함수명에 _(언더스코어)를 붙이는 이유? 리액트 자체 기능과 내가 만든 기능에 차이를 두기 위해서
   }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies 
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=like_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
--- movie-app/src/Movie.js
+++ movie-app/src/Movie.js
@@ -2,43 +2,50 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string,
-        poster: PropTypes.string
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
+function Movie({ title, poster, genres, synopsis }) {
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
             </div>
-        )
-    }
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                    <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">{synopsis}</p>
+            </div>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+function MoviePoster({ poster, alt }) {
+    return (
+        <img src={poster} alt={alt} className="Movie__Poster"></img>
+    );
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-
-    // componentWillMount(){
-    //     console.log('will mount');
-    // };
-
-    // componentDidMount(){
-    //     console.log('did mount');
-    // };
-
-    render() {
-        // console.log('did render');
-        return (
-            <img src={this.props.poster}></img>
-        );
-    }
 }
 
+function MovieGenre({ genre }) {
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+
+}
 export default Movie;
